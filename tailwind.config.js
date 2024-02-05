d8af7d4d2d4e06ea7fb58576a17879ae8d286355
Rafal <rafek131@wp.pl>
2024-02-05 14:45:27 +0100

    Project 1 - completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./dist/index.html'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Roboto', 'sans-serif'],
+			},
+			colors: {
+				darkSlateGrey: 'hsl(234, 29%, 20%)',
+				charcoalGrey: 'hsl(235, 18%, 26%)',
+				grey: 'hsl(231, 7%, 60%)',
+				tomato: 'hsl(4, 100%, 67%)',
+			},
+			backgroundImage: {
+				'pink-tomato-gradient': 'linear-gradient(90deg, #fe527a 0%, hsl(4, 100%, 67%) 100%)',
+			},
+		},
+	},
 	plugins: [],
 };
